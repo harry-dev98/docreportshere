a2a8eb68b2c9b031aa2edc67407524c5e8cdd9f2
harry-dev98 <harjeet.769644@gmail.com>
2021-01-11 16:47:15 +0530

    chatbox

--- client-side/src/components/chat/Chat.js
+++ client-side/src/components/chat/Chat.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import { Link, } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import ChatTag from './ChatComponent';
-export default class Chat extends Component{
+import {
+    openNewChatBox,
+    deleteChatBox,
+} from './action';
+import { ChatList, ChatBox } from './ChatComponent';
+class Chat extends Component{
     render(){
+        const userList = [
+            {
+                id: '1',
+                name: 'testuser1',
+                online: true,
+                domain: ['ENT', ],
+                avatar: '',
+            },
+            {
+                id: '2',
+                name : 'testuser2',
+                online: false,
+                domain: ['Skin', ],
+                avatar: '',
+            },
+        ];
         return (
-            <ChatTag />
+            <div className="chat-container">
+                <ChatList {...{userList}} openChat={this.props.newChatBox} />
+                {Object.entries(this.props.activeChats).map(([key, item]) => (
+                    <ChatBox key={key} chatInfo={item} />
+                ))}
+            </div>
         );
     }
 }
+
+const mapState = ({ chatState }) => ({
+    activeChats: chatState.activeChats,
+});
+const mapDispatch = (dispatch) => ({
+    newChatBox: (data)=>dispatch(openNewChatBox(data)),
+    delChatBox: (id)=>dispatch(deleteChatBox(id)),
+});
+
+export default connect(mapState, mapDispatch)(Chat);
--- client-side/src/components/chat/ChatComponent.jsx
+++ client-side/src/components/chat/ChatComponent.jsx
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { toggleChatBox, deleteChatBox } from './action';
 
 import './Chat.css';
 
-// const ChatList = (props) => (
-
-// );
-// const ChatBox = (props) => (
-
-// )
-const Chat = (props) => {
-    return (
-        <div className="chat-box">
-            <div className="chat-list"></div>
-            <div className="chat">
-
-            </div>
+const ChatHeader = ({ name, online, avatar, onClick, onCross }) => (
+    <div className="chat-header" onClick={onClick}>
+        { avatar && <img className="chat-avatar" src={avatar} />}
+        <div className="chat-heading"><div className="bold-text">{name}</div></div>
+        <div className="chat-actions">
+            <i className="fa fa-circle chat-status" style={{color: online?'green':'#042F19'}}></i>
+            {onCross && <i className="fa fa-times chat-close" style={{color: '#042F19'}} onClick={(event)=>{event.stopPropagation(); onCross()}}></i>}
         </div>
-    )
-}
+    </div>
+)
 
-export default Chat;
+const ChatList = ({ userList, openChat }) => {
+    const [active, setActive] = useState(false);
+
+    const ChatLabel = () => (
+        <div className="chat-label" onClick={()=>setActive(!active)}><div className="text text-center">Chat</div></div>
+    );
+    return (
+        <div className={"chat-list-container"+(active?' active':'')} onBlur={()=>setActive(false)}>
+            {active && <div className="chat-list">
+            {userList.map((item) => (
+                <ChatHeader key={item.id} {...item} onClick={()=>openChat(item)} />    
+                ))}
+            </div>}
+            <ChatLabel />
+        </div>
+    );
+};
+
+const ChatBox = ({ chatInfo }) => {
+    const inputRef = useRef();
+    const dispatch = useDispatch();
+    const active = useSelector((state)=>state.chatState.openChats[chatInfo.id]);
+    const [message, setMessage] = useState(undefined);
+    return (
+        <div className={"chat-box-container"+ (active?" active":"")}>
+            <ChatHeader {...chatInfo} 
+                onClick={()=>dispatch(toggleChatBox(chatInfo.id))}
+                onCross={()=>dispatch(deleteChatBox(chatInfo.id))}
+            />
+            {active && <><div className="chat-box">heh{message}</div>
+            <div className="chat-type">
+                <input type="text" name="type_message" id="chat-type" ref={inputRef}/>
+                <i className="fa fa-paper-plane" style={{color: '#042F19'}} onClick={()=>setMessage(inputRef.current.value)}></i>
+            </div></>}
+        </div>
+    );
+};
+
+export {
+    ChatBox,
+    ChatList,
+};
