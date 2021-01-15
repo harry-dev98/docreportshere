d8320a29450c450c2bae6dc1531697812ff352be
harry-dev98 <harjeet.769644@gmail.com>
2021-01-15 12:42:18 +0530

    apiends exported from single file

--- client-side/src/service/api.js
+++ client-side/src/service/api.js
@@ -1,5 +1,8 @@
 import {
-    addPatientAPI
+    addPatientAPI,
+    getPatientsAPI,
+    addScanAPI,
+    assignDoctorAPI,
 } from './patient';
 
 import {
@@ -7,8 +10,29 @@ import {
     signupAPI
 } from './login';
 
+import {
+    addReportAPI,
+    getDoctorsAPI,
+    approveDoctorAPI,
+} from './doctor';
+
+import {
+    newMessageAPI,
+    getMessagesAPI,
+    getNotificationsAPI,
+} from './message';
+
 export {
     addPatientAPI,
+    getPatientsAPI,
+    addScanAPI,
+    assignDoctorAPI,
     loginAPI, 
     signupAPI,
+    addReportAPI,
+    getDoctorsAPI,
+    approveDoctorAPI,
+    newMessageAPI,
+    getMessagesAPI,
+    getNotificationsAPI,
 };
